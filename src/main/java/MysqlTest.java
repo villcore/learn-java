f74fc9b55e99f2c4f57ec65390a07bf21b239881
villcore <villcore@163.com>
2017-03-13 09:05:07 +0800

    1.增加导出代码

---
+++ src/main/java/MysqlTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by villcore on 2017/2/21.
+ */
+public class MysqlTest {
+    public static void main(String[] args) {
+//        Class.forName("");
+//        DriverManager.getConnection("");
+//        System.out.println(Reflection.getCallerClass());
+    }
+}
